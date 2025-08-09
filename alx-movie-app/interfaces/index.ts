c1698e9120392cfb5ac111663544a62e4c5f5420
Mac <mac@okey101.local>
2025-08-09 21:33:15 +0100

    Add the movies pages to you project

--- alx-movie-app/interfaces/index.ts
+++ alx-movie-app/interfaces/index.ts
@@ -8,3 +8,29 @@ export interface ButtonProps {
     title: string,
     action?: () => void
 }
+
+export interface MovieProps {
+    id?: string,
+    title: string,
+    posterImage: string,
+    releaseYear: string
+}
+
+interface PrimaryImage {
+  url: string
+}
+
+interface TitleText {
+  text: string
+}
+
+interface ReleaseYear {
+  year: string
+}
+
+export interface MoviesProps {
+  id: string
+  primaryImage: PrimaryImage
+  titleText: TitleText
+  releaseYear: ReleaseYear
+}
